b8d79244d42c09055dd26814cbe6091e1a8b836a
Samuel Armes <s.armes.21@unimail.winchester.ac.uk>
2024-06-03 15:02:00 +0100

    Add project files.

---
+++ Waqqly/Pages/Shared/AddWalker.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Waqqly.Pages.Shared
+{
+    public class AddWalkerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
